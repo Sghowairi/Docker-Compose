4e7ee8c048b206b02c8ffe96b1cf5158c22edde2
sghowairi <shahadalghowairi@gmail.com>
2024-10-06 15:32:06 +0300

    Compos

---
+++ app/app.py
@@ -0,0 +1,29 @@
+mport os
+from flask import Flask
+import psycopg2
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get("POSTGRES_HOST"),
+        db=os.environ.get("POSTGRES_DB"),
+        user=os.environ.get("POSTGRES_USER"),
+        password=os.environ.get("POSTGRES_PASSWORD")
+    )
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f'Database connected: {db_version}'
+
+
+if name == '__main__':
+    app.run(debug=True, host='0.0.0.0')
